320565a6836cbedb6cda5dd46ebf37043cc57aff
Mercy Komen <mercytich@Mercys-MacBook.local>
2019-07-08 17:28:04 -0400

    tested the frequency count using NLTK

--- freq.py
+++ freq.py
@@ -2,30 +2,36 @@
 import nltk
 from nltk import word_tokenize,sent_tokenize
 from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+
+tokenizer = RegexpTokenizer(r'\w+')
+
+
+
 nltk.download('punkt')
 nltk.download('stopwords')
 
-text="""Hello Mr. Smith, how are you doing today? The weather is great, and city is awesome.
-The sky is pinkish-blue. You shouldn't eat cardboard"""
+#text="""Hello Mr. Smith, .. how are you doing today? The weather is great, and city is awesome.
+#The sky is pinkish-blue. You shouldn't eat cardboard"""
 
 with open('user.txt', 'r') as f:
-    data = f.read()
+    text = f.read()
 
 from nltk.tokenize import word_tokenize
-tokenized_word=word_tokenize(text)  
+tokenized_word=word_tokenize(text)   
+tokenized_word = [word for word in tokenized_word if word.isalpha()]
+
 
 from nltk.probability import FreqDist
 fdist = FreqDist(tokenized_word)
 
 stop_words=set(stopwords.words("english"))
-print(stop_words)
 
 filtered_sent=[]
 for w in tokenized_word:
     if w not in stop_words:
         filtered_sent.append(w)
-print("Tokenized Sentence:",tokenized_word)
-print("Filterd Sentence:",filtered_sent)
 
 
-#print(fdist.most_common(25))
+
+print(fdist.most_common(25))
